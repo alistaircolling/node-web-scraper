058177446be9ba12b637c09486dd57c23ecc2753
alistaircolling <alistair.colling@gmail.com>
2016-08-04 10:44:28 +0100

    working

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+//         Checks if an img element with specific class contains a specific jpg ///////////////
+
 var request = require("request");
 var cheerio = require("cheerio");
+const url = "https://www.alpkit.com/products/returned-lotic-wetsuit-mens";
+const elementType = 'img';
+const className = 'sold-out';
+const attribute = 'class';
+const jpgName = '/images/made/images/studio/lotic_seconds_men_80_60auto_c1_c_c.jpg';
 
 console.log('hello node scraper');
 
 request({
-    uri: "https://www.alpkit.com/products/returned-lotic-wetsuit-mens",
+    uri: url
 },
 
 function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log('success!');
             var $ = cheerio.load(body);
-            var hasSoldOut = $('img').attr('class', 'sold-out');
-            console.log(hasSoldOut);
+            var hasSoldOut = $($(elementType).attr(attribute, className)[15]).attr('src');
+            console.log(jpgName);
+            console.log('========== is contained in below? ==========');
+            console.log(hasSoldOut);    
+            console.log("index:"+hasSoldOut.indexOf(jpgName));
+            if (hasSoldOut.indexOf(jpgName) !== -1) {
+                console.log('has sold out 😕');
+            }else{
+                console.log('may be in stock 🙏');
+            }
         }else{
-            console.log('error');
+            console.log('error 😖');
         }
     }
 );
-
-//var cheerio = require("cheerio");
-//var html = "<ul><li>foo</li><li>bar</li></ul>";
-//var $ = cheerio.load(html);
-//var list = $("ul");
-
-//console.log(list.html());
